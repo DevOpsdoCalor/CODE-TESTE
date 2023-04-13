93e9338ad5fd75e2efd04797e8104b387ad47b34
Jackson <JACKSON_REDES@HOTMAIL.COM>
2023-04-13 14:37:59 -0400

    Script de execução do node advpl

---
+++ script.sh
@@ -0,0 +1,2 @@
+sudo docker run --rm --tty -v /tmp/includes/CODE-TESTE:/tmp -v /home/jackson/output/:/bin/output/ -v /home/jackson/conf:/bin/conf  totvsengpro/advpl-tlpp-code-analyzer
+#comando executado dentro do JENKINS
